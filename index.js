4231e8491a9f87d144811ab5e7627bb3e37025cb
Stephan Jess <s.jess@novagraphix.de>
2026-03-24 14:09:15 +0100

    🔧 FIX: Composer Installation modernisiert

--- index.js
+++ index.js
@@ -221,14 +221,7 @@ async function installPackages(title, pkgs) {
     // 9. Composer installation
     console.log(chalk.blue(`\n[ START ] Composer installieren`));
     t0 = Date.now();
-    runCommand("php -r \"copy('https://getcomposer.org/installer','composer-setup.php');\"", { ignoreOutput: true });
-    runCommand(
-        "php -r \"if(hash_file('sha384','composer-setup.php')==='dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6'){echo 'Installer verified';}else{echo 'Installer corrupt';unlink('composer-setup.php');}echo PHP_EOL;\"",
-        { ignoreOutput: false }
-    );
-    runCommand('php composer-setup.php', { ignoreOutput: true });
-    runCommand("php -r \"unlink('composer-setup.php');\"", { ignoreOutput: true });
-    runCommand('sudo mv composer.phar /usr/local/bin/composer', { ignoreOutput: true });
+    runCommand('curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer', { ignoreOutput: true });
     console.log(chalk.green(`[ DONE ] Composer installieren in ${Math.round((Date.now() - t0) / 1000)}s`));
 
     // 10. Laravel installer
